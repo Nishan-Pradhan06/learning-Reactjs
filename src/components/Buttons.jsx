2db3ed1e5e500e52eced4575576e426f63bfa75f
nishan <nishanpradhan500@gmail.com>
2023-12-04 20:51:09 +0545

    learn hooks-useState

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 // import './App.css'
+// style ={{backgroundColor : `${props.bgcolor}`}}
 const Buttons = (props) => {
     return (
         <div>
@@ -8,4 +10,12 @@ const Buttons = (props) => {
     )
 }
 
+//yati kunai pani props pathako xaina aru page bata vhane yasle yo deafult rakhdinxa
+Buttons.defaultProps = {
+    ButtonText: "Button"
+}
+// to check the proptypes and validate the props
+Buttons.propTypes = {
+    ButtonText: PropTypes.string
+}
 export default Buttons
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Buttons from '../components/Buttons'
 const HomePage = () => {
+
+  const [count, setCount] = useState(0) //hook useState
   return (
     <div>
       <h1>This is Homepage</h1>
       <Buttons ButtonText="HomePage" />
+
+      <h1>{count}</h1>
+      {/* <Buttons ButtonText="Count" onClick={() => setCount(count + 1)} /> */}
+      <button className='my-button' onClick={() => setCount(count + 1)}>Count</button>
+
+
     </div>
   )
 }
